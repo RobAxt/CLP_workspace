07cf8d72faefad270e6a0c36f7840e81df694c9e
RobAxt <roberto.axt@gmail.com>
2025-03-05 23:59:38 -0300

    first commit

---
+++ Ej00/Fuente/neg.vhd
@@ -0,0 +1,19 @@
+-- Bibliotecas
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+-- Declaración de Entidad
+entity neg is
+	port(
+		a_i: in std_logic;
+		b_o: out std_logic
+	);
+end;
+
+-- Declaracion Cuerpo de Arquitectura
+architecture neg_arq of neg is
+  -- Parte Declarativa
+begin
+  -- Parte Descriptiva
+	b_o <= not a_i;
+end;
---
+++ Ej00/Fuente/neg_tb.vhd
@@ -0,0 +1,33 @@
+-- Bibliotecas
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+-- Declaración de Entidad
+entity neg_tb is
+end;
+
+-- Declaracion Cuerpo de Arquitectura
+architecture neg_tb_arq of neg_tb is
+  -- Parte Declarativa
+  component neg is
+	  port(
+		  a_i: in std_logic;
+	  	b_o: out std_logic
+	  );
+  end component;
+  
+	signal a_tb: std_logic := '0';
+	signal b_tb: std_logic;
+	
+begin
+  -- Parte Descriptiva
+  
+	a_tb <= '1' after 150 ns, '0' after 400 ns;
+
+	DUT: entity work.neg
+		port map(
+			a_i => a_tb,
+			b_o => b_tb
+		);
+
+end;
